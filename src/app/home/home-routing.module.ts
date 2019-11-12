70782a368d2674ffeffc5d82bf9964461d8a23e3
Unknown <mikolaj.skoczylas@partners.mbank.pl>
2019-11-12 13:08:57 +0100

    Add componenets for each screan

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -4,11 +4,21 @@ import { Routes, RouterModule } from '@angular/router';
 import { extract } from 'app/core';
 import { HomeComponent } from './home.component';
 import { Shell } from 'app/shell/shell.service';
+import { E1 } from './E1/e1.component';
+import { E2 } from './E2/e2.component';
+import { E3 } from './E3/e3.component';
+import { E4 } from './E4/e4.component';
+import { E5 } from './E5/e5.component';
 
 const routes: Routes = [
   Shell.childRoutes([
     { path: '', redirectTo: '/home', pathMatch: 'full' },
-    { path: 'home', component: HomeComponent, data: { title: extract('Home') } }
+    { path: 'home', component: HomeComponent, data: { title: extract('Home') } },
+    { path: 'e1', component: E1 },
+    { path: 'e2', component: E2 },
+    { path: 'e3', component: E3 },
+    { path: 'e4', component: E4 },
+    { path: 'e5', component: E5 }
   ])
 ];
 
