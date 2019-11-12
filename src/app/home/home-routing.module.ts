6552fa5c3df414b3a0d0e7839f3ee7a52843ce6b
Tomasz Galus <internetum@gmail.com>
2019-11-12 22:38:55 +0100

    E4 tunning

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -8,17 +8,15 @@ import { E1 } from './E1/e1.component';
 import { E2 } from './E2/e2.component';
 import { E3 } from './E3/e3.component';
 import { E4 } from './E4/e4.component';
-import { E5 } from './E5/e5.component';
 import { FirstPost } from './first-post/first-post.component';
 
 const routes: Routes = [
   Shell.childRoutes([
-    { path: '', redirectTo: '/e1', pathMatch: 'full' },
+    { path: '', redirectTo: '/firstPost', pathMatch: 'full' },
     { path: 'e1', component: E1 },
     { path: 'e2', component: E2 },
     { path: 'e3', component: E3 },
     { path: 'e4', component: E4 },
-    { path: 'e5', component: E5 },
     { path: 'firstPost', component: FirstPost }
   ])
 ];
